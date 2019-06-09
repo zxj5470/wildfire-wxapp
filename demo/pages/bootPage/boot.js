00b4dd050e7684e6fd64c3998897089461f20897
zxj5470 <zxj5470@foxmail.com>
2019-06-09 09:45:03 +0800

    普通用户登录

--- demo/pages/bootPage/boot.js
+++ demo/pages/bootPage/boot.js
@@ -1,20 +1,51 @@
 // pages/bootPage/boot.js
+const app = getApp()
+const HOST = app.globalData.HOST
+const PORT = app.globalData.PORT
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
   },
 
-  toPerson: function(){
-    wx.navigateTo({
-
-    })
+  toPerson: function () {
+    //登录
+    wx.login({
+      success: function (res) {
+        //console.log(res.code);
+        if (res.code) {
+          wx.request({
+            url: `http://${HOST}:${PORT}/api/user/login`,
+            method: 'POST',
+            data: {
+              code: res.code,
+              userInfo: JSON.stringify(app.globalData.userInfo)
+            },
+            header: {
+              'content-type': 'application/x-www-form-urlencoded'
+            },
+            success(res) {
+              console.log(res.data.results);
+              console.log("跳转到普通用户的主界面");
+              // wx.redirectTo({
+              //   url:''
+              // })
+            }
+          })
+        } else {
+          console.log('获取用户登录态失败！' + res.errMsg)
+        }
+      }
+    });
   },
 
-  toGroup: function(){
+  toGroup: function () {
     wx.redirectTo({
       url: '../logInPage/logInPage'
     })
@@ -24,7 +55,41 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+    this.toPerson()
   },
 
   /**
